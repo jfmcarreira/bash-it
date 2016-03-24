8c6783d366142c3b3de8279807544650dcaa5ae0
Joao Carreira <jfmcarreira@gmail.com>
2016-03-24 13:52:22 +0000

    Fix some symbols

--- themes/carreira/carreira.theme.bash
+++ themes/carreira/carreira.theme.bash
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+SCM_THEME_PROMPT_DIRTY=
+SCM_THEME_PROMPT_CLEAN=
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=
+GIT_THEME_PROMPT_CLEAN=
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+SCM_THEME_BRANCH_TRACK_PREFIX=' -> '
+SCM_THEME_BRANCH_GONE_PREFIX=' -> '
+
 function cluster_name ()
 {
   if [ $USER == "lljfmc" ]
@@ -17,7 +33,7 @@ function cluster_name ()
 }
 
 function prompt_command() {
-  PS1="$(battery_char)$(clock_only)${yellow}$(ruby_version_prompt) ${bright_green}\u@\h$(cluster_name) ${bright_blue}\w ${green}$(scm_prompt_info)${bright_blue}\$${reset_color} "
+  PS1="\n$(battery_char)$(clock_only)${yellow}$(ruby_version_prompt) ${bright_green}\u@\h$(cluster_name) ${bright_blue}\w ${green}$(scm_prompt_info)${bright_blue}\$${reset_color} "
 }
 
-PROMPT_COMMAND=prompt_command;
+PROMPT_COMMAND="prompt_command;"
